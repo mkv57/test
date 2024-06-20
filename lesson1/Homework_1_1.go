4ac2d0775188e33a55d2ea8ceb5ab2a926ebe7f7
Konstantin <mkv2008@yandex.ru>
2024-06-20 21:36:41 +0300

    proba

---
+++ lesson1/Homework_1_1.go
@@ -0,0 +1,22 @@
+/*1. Объявление переменных: Создайте переменные различных типов (int,
+float64, string, bool) и присвойте им значения. Выведите на экран значения
+этих переменных.*/
+
+package main
+
+import "fmt"
+
+const w string = "Hello IT Academy"
+
+func main() {
+
+	fmt.Println(w)
+
+	var a int = 57
+	var b float64 = 21
+	var c string = "Have a good day"
+	var d bool = true
+
+	fmt.Println(c, "\n", a, "\n", b, "\n", d)
+
+} // git
